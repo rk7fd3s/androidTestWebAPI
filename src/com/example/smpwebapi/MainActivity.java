08b09def807e293c0df272c6c4200bc9745935d8
r-kinoshita <r-kinoshita@solcreo.co.jp>
2014-02-24 20:16:15 +0900

    menu delete

--- src/com/example/smpwebapi/MainActivity.java
+++ src/com/example/smpwebapi/MainActivity.java
@@ -58,9 +58,10 @@ public class MainActivity extends Activity implements HttpAsyncCallback {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return false;
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.main, menu);
+//		return true;
 	}
 
 	/**
